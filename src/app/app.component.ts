8e622e47098573077a31f4f92b8184d79740dde1
nirmala <nirmala@surf.net.np>
2019-01-04 11:36:25 +0100

    import components in app.module, add conditional rendering between two navigation that accepts navigation with type string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project-app';
+  navigationNameSelected: string;
+  onNavigate(navigationName: string) {
+    this.navigationNameSelected = navigationName;
+  }
 }
