3c3baebee4773d3bea28b1cc214bf074604dc806
nirmala <nirmala@surf.net.np>
2019-01-11 17:22:09 +0100

    inject router and activated router for navigating to realtive path

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -11,10 +12,18 @@ import { RecipeService } from '../recipe.service';
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {
-  }
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router, // to get the path of the route
+    private activeRoute: ActivatedRoute // to activate relative route
+    ) {}
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+  // the click event triggers onAddRecipe which uses route to navigate to the new recipe path
+  onAddNewRecipe() {
+    console.log(this.router.navigate(['new'], {relativeTo: this.activeRoute}));
+    this.router.navigate(['new'], {relativeTo: this.activeRoute}); // use JS object to point relative route
   }
 }
