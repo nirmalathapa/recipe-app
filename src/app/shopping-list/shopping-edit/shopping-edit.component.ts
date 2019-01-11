e0a1e6f4f9971c3ccb09dd594dcd397b120e46d7
nirmala <nirmala@surf.net.np>
2019-01-11 10:04:34 +0100

    move ingredient array list to shopping list service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list-service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,16 +8,17 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('inputName') nameInput: ElementRef;
-  @ViewChild('amountNumber') amountNumber: ElementRef;
-
-  constructor() { }
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountNumber') amountNumberRef: ElementRef;
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAdd(){
-
+  onAdd() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountNumberRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
-
-
 }
+
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list-service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Egg', 12),
-    new Ingredient('Tomatoes', 5),
-    new Ingredient('Basil', 2)
-  ];
-  constructor() { }
+  ingredients: Ingredient[];
+  constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientArrayEvent
+    .subscribe((ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+    });
   }
-
 }
