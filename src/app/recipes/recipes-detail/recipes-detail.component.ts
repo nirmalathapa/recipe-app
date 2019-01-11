6b36213f7dcd0568a00ae368436f88678a62b97b
nirmala <nirmala@surf.net.np>
2019-01-11 09:46:50 +0100

    remove manual data passing with @input and use service to pass the data, Activate route

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
 
 @Component({
   selector: 'app-recipes-detail',
@@ -7,10 +10,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-detail.component.css']
 })
 export class RecipesDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-  constructor() { }
+  recipe: Recipe;
+  id: number;
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
+
+  onAddIngredientToShoppingList() { // click event call on add ingredients function
+    // get access to the recipeservice method to the add the ingredients array
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
 }
