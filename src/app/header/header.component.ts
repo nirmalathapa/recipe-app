8e835f6701541f72373e8d6863712dc69b3f5160
nirmala <nirmala@surf.net.np>
2019-01-11 09:30:31 +0100

    remove custom events for the navigation links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() navigationEvent = new EventEmitter<string>(); // create custom navigation event emitter
-  navigationLink(navigationName: string) { // corresponds to the event binding that listens to the event passed from user
-    this.navigationEvent.emit(navigationName);
-  }
+
 }
